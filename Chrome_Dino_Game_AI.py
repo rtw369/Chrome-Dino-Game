da4600f30e984b56738bfbdac98b086612189bef
rtw369 <69614334+rtw369@users.noreply.github.com>
2022-09-16 15:19:53 -0700

    Updated codes
    - Implemented the codes for Dino class so the dino does not fall below the base
    - Created Base class

--- Chrome_Dino_Game_AI.py
+++ Chrome_Dino_Game_AI.py
@@ -44,14 +44,26 @@ class Dino:
         self.img = self.IMGS[0]
 
     def jump(self):
-        self.vel = -11
-        self.tick_count = 0
+        if self.y >= 500 - self.img.get_height() + 15:
+            self.vel = -12
+            self.tick_count = 0
         self.height = self.y
 
     def move(self):
         self.tick_count += 1
 
         d = self.vel * self.tick_count + 1.5 * self.tick_count ** 2
+
+        # makes the dino fall slower.
+        if d >= 12:
+            d = 12
+
+        if d < 0:
+            d -= 2
+
+        if d > 0 and self.y >= 500 - self.img.get_height() + 15:
+            self.vel = 0
+            d = 0
 
         self.y = self.y + d
 
@@ -66,15 +78,53 @@ class Dino:
             self.img = self.IMGS[0]
             self.img_count = 0
 
-        rect = self.img.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
-        win.blit(self.img, rect.topleft)
+        if self.y < 500 - self.img.get_height() + 15:
+            self.img = self.IMGS[2]
+
+        win.blit(self.img, (self.x, self.y))
 
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
 
-def draw_window(win, dino):
+class Base:
+    INIT_VEL = 10
+    MAX_VEL = 30
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.vel = self.INIT_VEL
+        self.tick_count = 0
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.tick_count += 1
+        self.vel = self.INIT_VEL + self.tick_count * 0.05
+
+        if self.vel > self.MAX_VEL:
+            self.vel = self.MAX_VEL
+
+        self.x1 -= self.vel
+        self.x2 -= self.vel
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+
+def draw_window(win, dino, base):
     win.blit(BG_IMG, (0, 0))
+
+    base.draw(win)
 
     dino.draw(win)
 
@@ -82,7 +132,8 @@ def draw_window(win, dino):
 
 
 def main():
-    dino = Dino(100, 100)
+    base = Base(500)
+    dino = Dino(100, 500 - DINO_IMGS[0].get_height() + 15)
     clock = pygame.time.Clock()
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
@@ -97,7 +148,11 @@ def main():
 
         dino.move()
 
-        draw_window(win, dino)
+        dino.jump()
+
+        base.move()
+
+        draw_window(win, dino, base)
 
 
 main()
